b2497d154cd58857b3fd9b3df468526a88f817d8
valentimdev <valentimdesenvolvedor@gmail.com>
2025-11-06 14:39:32 -0300

    estrutura de divs na App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import React from 'react';
 
 function App() {
-
   return (
     <>
+      <div className='flex justify-center items-center w-full h-full border border-white p-10'>
+        
+        <div className='flex w-full h-full border border-white'>
+          
+          <div className="border border-amber-400 h-full w-45">
+          </div>
+      
+  
+          <div className="flex flex-col flex-1 h-full">
+            
+            <div className="border border-blue-50 w-full h-30">
+            </div>
+            
+            <div className="flex-1 border border-green-500 w-full">
+            </div>
+
+          </div>
+        </div>
+        
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
